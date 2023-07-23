ebf49d5ff722d2508f8b2c28a52fafaf6e08a265
MateoCondor <mjcondor2@espe.edu.ec>
2023-07-23 18:58:24 -0500

    Añadido parte de luis

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const register = document.getElementById("register");
+const login = document.getElementById("login");
+const container = document.getElementById("container");
+
+register.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+login.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
