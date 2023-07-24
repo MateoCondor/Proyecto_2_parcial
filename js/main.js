da7594554772d01733cb7908ce2d35bd3748ac29
LuisBurbano <laburbano1@espe.edu.ec>
2023-07-23 22:57:43 -0500

    inicio validado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 const register = document.getElementById("register");
 const login = document.getElementById("login");
 const container = document.getElementById("container");
+const usuarioBase ="admin";
+const passwordBase ="1234";
+function validarNombre() {
+  let expRegNombre = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let nombreControl = false;
+  let nombre = "";
+
+  nombre = document.getElementById("usuario").value;
+  password = document.getElementById("password").value;
+  if (nombre.length == 0 || !expRegNombre.exec(nombre)) {
+    alert("Solo se admiten letras y espacios");
+    
+  } 
+  if (password.length == 0) {
+    alert("Ingrese una contraseña");
+  }else{
+    if (usuarioBase==nombre && passwordBase==password) {
+      nombreControl=true;
+    } else {
+      nombreControl=false;
+      alert("Usuario o contraseña incorrectos");
+    }
+  }
+  return nombreControl;
+}
+
+const loginButton = document.getElementById("iniciarSesionLogin");
 
 register.addEventListener("click", () => {
   container.classList.add("right-panel-active");
@@ -8,4 +35,23 @@ register.addEventListener("click", () => {
 
 login.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  
+});
+
+function inciarSesion() {
+  
+  if (validarNombre()) {
+    alert("Bienvenido");
+    window.location.href = "http://" + document.domain + "/inicio_administrador.html";
+  }
+  
+  
+  
+}
+
+document.getElementById("enlaceIniciarSesion").addEventListener("click", function() {
+  inciarSesion();
 });
